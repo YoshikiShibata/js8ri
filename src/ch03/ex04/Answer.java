883aef1fb34d5720531a3548d2824217130a98e2
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2014-10-13 16:49:56 +0900

    ex04 redone

---
+++ src/ch03/ex04/Answer.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2014 Yoshiki Shibata. All rights reserved.
+ */
+package ch03.ex04;
+
+/**
+ *
+ * How many functional interfaces with Filter in their name can you find in the
+ * Java API? Which ones add value over Predicate<T>?
+ *
+ * 名前の中にFilterを含む関数型インタフェースが、Java API にはいくつありますか。
+ * そのうちのどれが、Predicate<T>よりも付加価値がありますか。
+ */
+public class Answer {
+
+    /*
+     * java.io.FileFilter
+     * java.io.FilenameFilter
+     * java.util.logging.Filter
+     * avax.imageio.spi.ServiceRegistry
+     * javax.management.NotificationFilter
+     * javax.xml.stream.EventFilter
+     * javax.xml.stream.StreamFilter
+     * org.xml.sax.XMLFilter
+     *
+     * Only FilenameFilter's method has two arguments instead of one.
+     */
+}
