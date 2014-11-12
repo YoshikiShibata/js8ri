278c28e5cbe6d2d59b46d0f85f584b493ff203ba
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2014-11-12 18:48:04 +0900

    backup

---
+++ src/ch03/ex16/ExceptionUtil.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2014 Yoshiki Shibata. All rights reserved.
+ */
+package ch03.ex16;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+/**
+ * Implement the doInOrderAsync of Section 3.8, “Dealing with Exceptions,” on
+ * page 58, where the second parameter is a BiConsumer<T, Throwable>. Provide a
+ * plausible use case. Do you still need the third parameter?
+ *
+ * 71 ページの3.8 節「例外の取り扱い」のdoInOrderAsync を実装し、2 つ目のパラメー
+ * タはBiConsumer<T, Throwable>としなさい。うまいユースケースを示しなさい。 3 つ目のパラメータは必要ですか。
+ */
+public class ExceptionUtil {
+
+    private ExceptionUtil() {
+        // Non-instantiable
+    }
+    
+    /**
+     * Executes supplier and consumer in order. The result of supplier will
+     * be provided to the consumer. If the supplier throws an exception, the
+     * exception will be passe to the consumer as the second parameter: in this
+     * case, the first parameter will be null. If the consumer will throws
+     * an Exception, then handler will be invoked.
+     * 
+     * @param <T> type varaible 
+     * @param supplier Supplier
+     * @param consumer Consumer
+     * @param handler Handle to be invoked when the consumer will throw an exception.
+     */
+    public static <T> void doInOrderAsync(
+            Supplier<T> supplier, BiConsumer<T, Throwable> consumer,
+            Consumer<Throwable> handler) {
+        Thread t = new Thread() {
+            
+            @Override
+            public void run() {
+                T result = null;
+                Throwable firstFailure = null;
+
+                try {
+                    result = supplier.get();
+                } catch (Throwable t) {
+                    firstFailure = t;
+                    try {
+                        consumer.accept(null, t);
+                    } catch (Throwable e) {
+                        handler.accept(e);
+                    }
+                }
+
+                if (firstFailure == null) {
+                    try {
+                        consumer.accept(result, null);
+                    } catch (Throwable t) {
+                        handler.accept(t);
+                    }
+                }
+            }
+        };
+        t.start();
+    }
+
+}
---
+++ test/ch03/ex16/ExceptionUtilTest.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2014 Yoshiki Shibata. All rights reserved.
+ */
+package ch03.ex16;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author yoshiki
+ */
+public class ExceptionUtilTest {
+
+    private static class AsyncResult {
+        private Boolean result = null;
+        
+        synchronized boolean waitForResult() {
+            while (result == null)
+                try {
+                    wait();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(ExceptionUtilTest.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            return result;
+        }
+        
+        synchronized void setResult(boolean result) {
+            this.result = result;
+            notifyAll();
+        }
+    }
+
+    public ExceptionUtilTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void bothOks() {
+        AsyncResult as = new AsyncResult();
+        
+        ExceptionUtil.doInOrderAsync(
+                () -> "Hello",  (t, e) -> {
+                    if (e != null) {
+                        as.setResult(false);
+                        return;
+                    }
+                    
+                    as.setResult(t.equals("Hello"));
+                }, null);
+        
+        assertTrue(as.waitForResult());
+    }
+    
+    @Test
+    public void firstException() {
+        AsyncResult as = new AsyncResult();
+        
+        ExceptionUtil.doInOrderAsync(
+            () -> {throw new RuntimeException("ha ha");},
+            (t, e) -> {
+                if (t != null) {
+                    as.setResult(false);
+                    return;
+                }
+                as.setResult(e != null);
+            }, null
+            );
+    }
+    
+    @Test
+    public void secondException() {
+        fail("Not Implemented Yet");
+    }
+    
+    @Test
+    public void firstExceptionThenSecondException() {
+        fail("Not Implemented Yet");
+    }
+}
