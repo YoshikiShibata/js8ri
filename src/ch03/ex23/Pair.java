daf8faae37c0405b033d733d81a0c80a303d1777
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2014-12-11 05:54:00 +0900

    ex23 done

---
+++ src/ch03/ex23/Pair.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2014 Yoshiki Shibata. All rights reserved.
+ */
+package ch03.ex23;
+
+import java.util.function.Function;
+
+/**
+ * Define a map operation for a class Pair<T> that represents a pair of objects
+ * of type T.
+ *
+ * T 型の対となる2 つのオブジェクトを表すPair<T>クラスに対するmap 操作を定義し なさい。
+ */
+public class Pair<T> {
+    private final T first;
+    private final T second;
+    
+    public Pair(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+    
+    public T first() {
+        return first;
+    }
+    
+    public T second() {
+        return second;
+    }
+    
+    public <U> Pair<U> map(Function<T, ? super U> f) {
+        return new Pair(f.apply(first), f.apply(second));
+    }
+
+}
