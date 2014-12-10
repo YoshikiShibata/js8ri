cd0b9c630e7845f6822478e0ba0e1b305f239f9e
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2014-12-11 05:56:33 +0900

    forgot to add

---
+++ test/ch03/ex23/PairTest.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2014 Yoshiki Shibata. All rights reserved.
+ */
+package ch03.ex23;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Test code for Pair
+ */
+public class PairTest {
+
+    @Test
+    public void testMap() {
+        Pair<String> p = new Pair("10", "20");
+        Pair<Integer> pi = p.map(Integer::valueOf);
+        
+        assertTrue(pi.first() == 10);
+        assertTrue(pi.second() == 20);
+    }
+}
