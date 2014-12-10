7b9b252505de9b6aa23a97ad1af4e0f39abc6288
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2014-12-10 18:32:24 +0900

    ex22 done

---
+++ src/ch03/ex22/Answer.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2014 Yoshiki Shibata. All rights reserved.
+ */
+package ch03.ex22;
+
+/**
+ * Is there a flatMap operation for CompletableFuture? If so, what is it?
+ * 
+ * ompletableFuture に対するflatMap 操作は存在しますか。存在するなら、それ
+は何ですか。
+ */
+public class Answer {
+    // There is no faltMap operation.
+}
