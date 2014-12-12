cf051d5eda2de80d05a51bf2543f35b367715f94
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2014-12-13 07:18:32 +0900

    added

---
+++ src/ch03/ex23/Answer.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2014 Yoshiki Shibata. All rights reserved.
+ */
+package ch03.ex23;
+
+/**
+ * Can you define a flatMap method for Pair<T>? If so, what is it? If not, why
+ * not?
+ *
+ * Pair<T>に対するflatMap メソッドを定義することができますか。できるとしたら、 それは何ですか。できないとしたら、その理由は何ですか。
+ */
+public class Answer {
+    
+    /*
+    flatMap would be like this:
+    
+    <R> Pair<R> flatMap(Function<? extends T, Pair<R>> f)
+    
+    There are two values: first and second. For each value, a Pair will be 
+    created. In other words, two Pair will be created; only one Pair cannot be returned.
+    */
+
+}
