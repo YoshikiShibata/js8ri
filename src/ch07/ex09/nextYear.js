8288898c0cdea2aaa394bc01280f26541321dde3
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2015-01-03 13:11:53 +0900

    ex09 done

---
+++ src/ch07/ex09/nextYear.js
@@ -0,0 +1,12 @@
+// Copyright (C) 2015 Yoshiki Shibata. All rights reserved.
+
+// run as jjs nextYear.js -- 55
+
+if (arguments.length == 1) {
+    print('Next year, you will be ' + (parseInt(arguments[0]) + 1))
+} else if ('AGE' in $ENV) {
+    print('Next year, you will be ' + (parseInt($ENV.AGE) + 1))
+} else {
+    print('Current Age is neither specified nor found')
+}
+        
