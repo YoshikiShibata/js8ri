60af68ca49a33489a0282f2628e19f5a47275b15
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2015-05-10 06:45:49 +0900

    added script

--- src/ch08/ex13/TestCaseAnnotationProcessor.java
+++ src/ch08/ex13/TestCaseAnnotationProcessor.java
@@ -3,10 +3,32 @@
  */
 package ch08.ex13;
 
+import java.util.Set;
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+
 /**
+ * Repeat the preceding exercise, but build a source-level annotation processor
+ * emitting a program that, when executed, runs the tests in its main method.
+ * (See Horstmann and Cornell, Core Java, 9th Edition, Volume 2, Section 10.6
+ * for an introduction into processing source-level annotations.)
  *
- * @author yoshiki
+ * 練習問題12 を繰り返しなさい。ただし、ソースレベルのアノテーションプロセッサを 構築しなさい。そのプロセッサは実行されると、main
+ * メソッドでテストを実行するプ ログラムを生成します（ソースレベルのアノテーション処理については、Horstmann、 Cornell 著、Core
+ * Java, 9th Edition, Volume 2 の10.6 節を参照してください）。
  */
-public class TestCaseAnnotationProcessor {
-    
+@SupportedAnnotationTypes({"ch08.ex13.TestCase", "ch08.ex13.TestCases"})
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+public class TestCaseAnnotationProcessor extends AbstractProcessor {
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
---
+++ src/ch08/ex13/run_processor.bash
@@ -0,0 +1,3 @@
+#/bin/bash
+javac -cp /Users/yoshiki/NetBeansProjects/js8ri_ex/build/classes -processor ch08.ex13.TestCaseAnnotationProcessor SampleAnnotatedClass.java
+
