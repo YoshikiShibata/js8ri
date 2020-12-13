0fe64cbeb9ed86d2fc4c5c49efb48a54e61db809
Yoshiki Shibata <yoshikishibata@YoshikinoMacBook-Pro.local>
2020-12-13 18:26:40 +0900

    Updated

--- src/ch05/ex01/ProgrammersDay.java
+++ src/ch05/ex01/ProgrammersDay.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Yoshiki Shibata. All rights reserved.
+ * Copyright (C) 2014, 2020 Yoshiki Shibata. All rights reserved.
  */
 package ch05.ex01;
 
@@ -9,15 +9,17 @@ import java.time.Period;
 /**
  * Compute Programmer’s Day without using plusDays.
  *
- * plusDays を使用しないで、プログラマーの日を計算しなさい。
+ * <p>plusDays を使用しないで、プログラマーの日を計算しなさい。
  */
 public class ProgrammersDay {
 
-    public static void main(String[] args) {
-        LocalDate programmersDay = LocalDate.of(2014, 1, 1).plusDays(255);
-        System.out.println(programmersDay);
-        
-        programmersDay = LocalDate.of(2014, 1, 1).plus(Period.ofDays(255));
-        System.out.println(programmersDay);
-    }
+  public static void main(String[] args) {
+    int thisYear = LocalDate.now().getYear();
+
+    LocalDate programmersDay = LocalDate.of(thisYear, 1, 1).plusDays(255);
+    System.out.println(programmersDay);
+
+    programmersDay = LocalDate.of(thisYear, 1, 1).plus(Period.ofDays(255));
+    System.out.println(programmersDay);
+  }
 }
