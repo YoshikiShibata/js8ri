89d96f5de0e8861cc56e2d95a8dd205aa5164052
Yoshiki Shibata <yoshikishibata@YoshikinoMacBook-Pro.local>
2020-12-13 19:27:18 +0900

    updated

--- src/ch06/ex10/ShowLinks.java
+++ src/ch06/ex10/ShowLinks.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Yoshiki Shibata. All rights reserved.
+ * Copyright (C) 2015, 2020 Yoshiki Shibata. All rights reserved.
  */
 package ch06.ex10;
 
@@ -18,79 +18,76 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Write a program that asks the user for a URL, then reads the web page at that
- * URL, and then displays all the links. Use a CompletableFuture for each stage.
- * Don’t call get. To prevent your program from terminating prematurely, call
+ * Write a program that asks the user for a URL, then reads the web page at that URL, and then
+ * displays all the links. Use a CompletableFuture for each stage. Don’t call get. To prevent your
+ * program from terminating prematurely, call
+ *
  * <pre>
  * ForkJoinPool.commonPool().awaitQuiescence(10, TimeUnit.SECONDS);
  * </pre>
  */
 public final class ShowLinks {
 
-    public static void main(String[] args) {
-        validateArgs(args);
+  public static void main(String[] args) {
+    validateArgs(args);
 
-        try {
-            URL url = new URL(args[0]);
-            CompletableFuture.
-                    supplyAsync(() -> blockingReadPage(url)).
-                    thenApply(ShowLinks::getLinks).
-                    thenAccept((lists) -> {
-                        for (URL u : lists) {
-                            System.out.println(u);
-                        }
-                    });
-        } catch (MalformedURLException ex) {
-            Logger.getLogger(ShowLinks.class.getName()).log(Level.SEVERE, null, ex);
-            showUsage();
-            System.exit(1);
-        }
-
-        ForkJoinPool.commonPool().awaitQuiescence(10, TimeUnit.SECONDS);
+    try {
+      URL url = new URL(args[0]);
+      CompletableFuture.supplyAsync(() -> blockingReadPage(url))
+          .thenApply(ShowLinks::getLinks)
+          .thenAccept(
+              (lists) -> {
+                for (String u : lists) {
+                  System.out.println(u);
+                }
+              });
+    } catch (MalformedURLException ex) {
+      Logger.getLogger(ShowLinks.class.getName()).log(Level.SEVERE, null, ex);
+      showUsage();
+      System.exit(1);
     }
 
-    private static void validateArgs(String[] args) {
-        if (args.length != 1) {
-            showUsage();
-            System.exit(1);
-        }
-        if (!(args[0].startsWith("http://"))) {
-            showUsage();
-            System.exit(1);
-        }
-    }
+    ForkJoinPool.commonPool().awaitQuiescence(10, TimeUnit.SECONDS);
+  }
 
-    private static void showUsage() {
-        System.out.printf("ShowLinks <URL>%n  URL: must start with http://%n");
+  private static void validateArgs(String[] args) {
+    if (args.length != 1) {
+      showUsage();
+      System.exit(1);
     }
-
-    private static String blockingReadPage(URL url) {
-        try {
-            Scanner in = new Scanner(url.openStream());
-            StringBuilder builder = new StringBuilder();
-            while (in.hasNextLine()) {
-                builder.append(in.nextLine());
-                builder.append("\n");
-            }
-            return builder.toString();
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
-        }
+    if (!args[0].startsWith("http://") && !args[0].startsWith("https://")) {
+      showUsage();
+      System.exit(1);
     }
+  }
 
-    private static List<URL> getLinks(String page) {
-        String hrefPattern = "<a\\s+href\\s*=\\s*\"([^\"]*)\"\\s*>";
-        Pattern pattern = Pattern.compile(hrefPattern, Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(page);
-        List<URL> result = new ArrayList<>();
+  private static void showUsage() {
+    System.out.printf("ShowLinks <URL>%n  URL: must start with http://%n");
+  }
 
-        while (matcher.find()) {
-            try {
-                result.add(new URL(matcher.group(1)));
-            } catch (MalformedURLException ex) {
-                Logger.getLogger(ShowLinks.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        return result;
+  private static String blockingReadPage(URL url) {
+    try {
+      Scanner in = new Scanner(url.openStream());
+      StringBuilder builder = new StringBuilder();
+      while (in.hasNextLine()) {
+        builder.append(in.nextLine());
+        builder.append("\n");
+      }
+      return builder.toString();
+    } catch (IOException ex) {
+      throw new RuntimeException(ex);
     }
+  }
+
+  private static List<String> getLinks(String page) {
+    String hrefPattern = "<a.*?href\\s*?=\\s*?[\"\']([^\"\']*)[\"\'].*?[/]*>";
+    Pattern pattern = Pattern.compile(hrefPattern, Pattern.CASE_INSENSITIVE);
+    Matcher matcher = pattern.matcher(page);
+    List<String> result = new ArrayList<>();
+
+    while (matcher.find()) {
+      result.add(matcher.group(1));
+    }
+    return result;
+  }
 }
